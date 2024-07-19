72fedcf654301f23db14f9c56e1a259d9b784f3f
slr7792301 <59087577+slr7792301@users.noreply.github.com>
2024-07-19 15:01:15 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
     <div className='App'>
       <Header />
       <Routes>
-        <Route path='SmrWeb/' element={ <Home/> } />
-        <Route path='SmrWeb/commission' element={ <Commission/> } />
+        <Route path='/SmrWeb/' element={ <Home/> } />
+        <Route path='/SmrWeb/commission' element={ <Commission/> } />
       </Routes>
     </div>
   );
